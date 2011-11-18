976dfa776497d76a0f713f607f192f0e5d571805
Mike Dalessio <mike@csa.net>
2011-11-18 08:04:42 -0500

    Skeleton specs for PivotalTrackerProject.

---
+++ spec/pivotal_tracker_project_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe ReleaseMarker::PivotalTrackerProject do
+  describe ".new" do
+    it "requires a string name and a hash of attributes as arguments"
+  end
+
+  describe "#changelog" do
+    it "returns an array of multi-line strings representing delivered tracker stories"
+  end
+end
